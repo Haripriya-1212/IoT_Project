2c32fe6cd57851be5afb6b8d04564b658714e19f
Ian Tauro <sushruth127@gmail.com>
2024-06-13 14:25:32 +0530

    Finished with implementation of queues completely

--- raspberry/src/main.py
+++ raspberry/src/main.py
@@ -1,102 +1,88 @@
-from io import BytesIO
-from time import sleep
+import pika, sys, os
 from threading import Thread
+from time import sleep
+from io import BytesIO
 
-# from picamera import PiCamera
-# from gpiozero import Buzzer
 from dotenv import load_dotenv
 
-import pika
-import os
+from picamera import PiCamera
+from gpiozero import Buzzer
 
 load_dotenv()
 
-# buzzer = Buzzer(17)
-i = 1
-
-def poll_camera_capture():
-    global i, channel
-    # capture_stream = BytesIO()
-    # camera = PiCamera()
-    # camera.start_preview()
-    # sleep(2)
-    print("stared polling camera capture")
-    while(True):
-        # camera.capture(capture_stream, 'jpeg')
-        # capture_stream.seek(0)
-        # channel.basic_publish(exchange='', routing_key='video', body=capture_stream.read())
-        print(f"Published Frame: {i}")
-        channel.basic_publish(exchange='', routing_key='video', body=f"[Video Frame Test] Count: {i}")
-        i = i + 1
-        sleep(1)
-
-
-# def ring_buzzer():
-#     global buzzer
-#     buzzer_time = 2
+def main():
+    buzzer = Buzzer(17)
     
-#     buzzer.on()
-#     sleep(buzzer_time)
-#     buzzer.off()
+    url = os.getenv("CLOUDAMQP_URL")
+    params = pika.URLParameters(url)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+
+    channel.queue_declare(queue='video')
+    channel.queue_declare(queue='speed')
+    channel.queue_declare(queue='report')
+
+    i = 1
+    def poll_camera_capture():
+        nonlocal i, channel
+
+        capture_stream = BytesIO()
+        camera = PiCamera()
+        camera.start_preview()
+
+        sleep(2)
+        print("Stared polling Camera Capture")
+
+        while(True):
+            camera.capture(capture_stream, 'jpeg')
+            capture_stream.seek(0)
+            channel.basic_publish(exchange='', routing_key='video', body=capture_stream.read())
+            print(f"Published Frame: {i}")
+            i = i + 1
+            sleep(1)
 
 
-current_speed = 20
+    def ring_buzzer():
+        nonlocal buzzer
+        buzzer_time = 4
+        
+        buzzer.on()
+        sleep(buzzer_time)
+        buzzer.off()
+        
+    # REMOVE THIS LINE WHEN GPS IS IMPLEMENTED
+    current_speed = 20
 
-def speed_callback(ch, method, properties, body):
-    global current_speed
-    
-    speed_limit = int(body)
+    def speed_callback(ch, method, properties, body):
+        nonlocal current_speed, channel
+        
+        speed_limit = int(body)
+        print(f"Received Speed Limit: {speed_limit}")
 
-    print("Received Speed Limit")
-    print(speed_limit)
+        # GET CURRENT SPEED FROM GPS HERE INSTEAD AND JUST ASSIGN IT TO current_speed
+        current_speed += 1
 
-    current_speed += 1
-
-    if current_speed > speed_limit:
-        # ring_buzzer()
-        channel.basic_publish(exchange='', routing_key='report', body='Speeding Found!')
-        current_speed = 0
-
-
-# RabbitMQ Setup
-
-def on_channel_open(chn):
-    global channel, capture_thread
-
-    channel = chn
-
-    chn.queue_declare(queue='video')
-    chn.queue_declare(queue='speed')
-    chn.queue_declare(queue='report')
-
-    chn.basic_consume(queue='speed', on_message_callback=speed_callback, auto_ack=True)
-
-    capture_thread = Thread(target=poll_camera_capture)
-    capture_thread.start()
+        if current_speed > speed_limit:
+            ring_buzzer()
+            channel.basic_publish(exchange='', routing_key='report', body='Speeding Found!')
+            current_speed = 0 # REMOVE THIS LINE WHEN GPS IS IMPLEMENTED
 
 
-def on_close(connection):
-    global capture_thread
-    capture_thread.join()
+    channel.basic_consume(queue='speed', on_message_callback=speed_callback, auto_ack=True)
 
+    thread = Thread(target=poll_camera_capture)
+    thread.start()
 
-def on_open(connection):
-    connection.channel(on_open_callback=on_channel_open)
-    connection.add_on_close_callback(on_close)
-
-
-url = os.getenv("CLOUDAMQP_URL")
-# params = pika.URLParameters(url)
-params = pika.ConnectionParameters('localhost')
-connection = pika.SelectConnection(params, on_open_callback=on_open)
-channel = None
-capture_thread = None
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
 if __name__ == '__main__':
     try:
-        print("Running PiSpeedCam Server Thread!")
-        connection.ioloop.start()
+        main()
     except KeyboardInterrupt:
-        print('Finished Running')
-        connection.close()
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- server/src/receive.py
+++ server/src/receive.py
@@ -6,19 +6,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    url = os.getenv("CLOUDAMQP_URL")
+    params = pika.URLParameters(url)
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
     channel.queue_declare(queue='video')
     channel.queue_declare(queue='speed')
     channel.queue_declare(queue='report')
 
+
     def video_callback(ch, method, properties, body):
         print(f"Received Video Frame: {body}")
+
+        # USE ML MODEL TO CHECK IF FRAME HAS SPEED SIGN
+        # IF NO, RETURN
+
+        # USE OPENCV TO EXTRACT SPEED LIMIT FROM FRAME AND PUBLISH IT
         channel.basic_publish(exchange='', routing_key='speed', body='80')
+
 
     def report_callback(ch, method, properties, body):
         print(f"Received Report: {body}")
+        # USE EMAIL TO SEND REPORT TO EMAIL AND STORE IT IN DATABASE
+
 
     channel.basic_consume(queue='video', on_message_callback=video_callback, auto_ack=True)
     channel.basic_consume(queue='report', on_message_callback=report_callback, auto_ack=True)
